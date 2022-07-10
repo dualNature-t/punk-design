6ae23b5fc8fe7a00c8d62cf4bd795f4591743b3e
1993765860@qq.com <1993765860@qq.com>
2022-07-10 13:59:03 +0800

    :tada: init 2022/7/10

---
+++ README.md
@@ -0,0 +1,21 @@
+# Usage
+
+## Local Environment Requirement
+
+```javascript
+    npm install react
+```
+
+## Install All The Necessary Package To The Project
+
+This project also need typescript to load. Please install typescript to use this component library.
+
+```javascript
+    npm install typescript
+```
+
+## Install this component library in your project.
+
+```javascript
+    npm install magical-design
+```
---
+++ index.ts
@@ -0,0 +1,13 @@
+import { Button } from "./Components/Button";
+import { Calendar} from "./Components/Calendar"
+import { DatePicker} from "./Components/DatePicker"
+import { Dropdown} from "./Components/Dropdown"
+import { Icon} from "./Components/Icon"
+import { Menu} from "./Components/Menu"
+import { Notice} from "./Components/Notice"
+import { Pagination} from "./Components/Pagination"
+import { ScrollBar} from "./Components/ScrollBar"
+import { Switch} from "./Components/Switch"
+import { Table} from "./Components/Table"
+
+export { Button, Calendar, DatePicker, Dropdown, Icon, Menu, Notice, Pagination, ScrollBar, Switch, Table };
